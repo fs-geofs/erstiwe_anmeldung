8ee4e3c06ab9f2e1e9b00b06d1f02f17946fdbab
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-01 17:00:08 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ErstiweAnmeldung::Application.config.secret_key_base = '4c21c21771e0f45da21f3cb5069965f13e779d28e728294cbc6378c3b35ab57a3fee6e8178d418c0ab6bfb19f5d4906fe605cfb819ef6300f92976587649cd7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ErstiweAnmeldung::Application.config.session_store :cookie_store, key: '_erstiwe_anmeldung_session'
