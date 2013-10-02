c9b3f23e99c584d4366feecf76d4631d81c9b7e6
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-02 13:17:03 +0200

    Registration Form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,27 @@
 class RegistrationsController < ApplicationController
-  def new
+
+  def show
+    @user = Registration.find(params[:id])
   end
+
+  def new
+  	@registration = Registration.new
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  	def registration_params
+      params.require(:registration).permit(:fname, :lname, :email, :gender, 
+      	:street, :streetno, :zip, :place, :birthday, :study, :vegetarian, :vegan, :ticket, :phone, :comment)
+    end  	
 end
