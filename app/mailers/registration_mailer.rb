edc7c4b7101201eee4bc29b1cdcab22496803200
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-04 12:13:51 +0200

    subject of registration complete mail

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -7,7 +7,7 @@ class RegistrationMailer < ActionMailer::Base
     @begin = Time.new(2013,11,1,9,30,0)
     @end = Time.new(2013,11,3,13,0,0)
     mail(to: @registration.email,
-     subject: 'Anmeldung Erstiwochenende todo - Daten vervollständigt',
+     subject: "Anmeldung Erstiwochenende #{begin.year} - Daten vervollständigt",
      content_type: "text/plain").deliver
   end
 
