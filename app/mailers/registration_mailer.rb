fb10b35d34fcd9ef69400f76c413142a6ca60c86
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-04 12:12:20 +0200

    Mail text tweaks

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,8 +1,11 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "fsgi@uni-muenster.de"
+  helper RegistrationMailerHelper
 
   def registration_complete_mail(registration)
     @registration = registration
+    @begin = Time.new(2013,11,1,9,30,0)
+    @end = Time.new(2013,11,3,13,0,0)
     mail(to: @registration.email,
      subject: 'Anmeldung Erstiwochenende todo - Daten vervollständigt',
      content_type: "text/plain").deliver
