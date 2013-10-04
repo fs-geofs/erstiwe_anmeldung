d29706954b848582e934b3a77337d6f838c03e71
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-04 10:37:32 +0200

    mailer with view for registration complete

---
+++ app/mailers/registration_mailer.rb
@@ -0,0 +1,11 @@
+class Registration  Mailer < ActionMailer::Base
+  default from: "fsgi@uni-muenster.de"
+
+  def registration_complete_mail(registration)
+    @registration = registration
+    mail(to: @registration.email,
+     subject: 'Anmeldung Erstiwochenende todo - Daten vervollständigt',
+     content_type: "text/plain").deliver
+  end
+
+end
