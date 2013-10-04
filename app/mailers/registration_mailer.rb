2978fb101949c5b0fbbc5e64ab33bf256f1d2de4
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-04 15:24:49 +0200

    redeem mail, redeem form, renamed actions

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -7,7 +7,15 @@ class RegistrationMailer < ActionMailer::Base
     @begin = Time.new(2013,11,1,9,30,0)
     @end = Time.new(2013,11,3,13,0,0)
     mail(to: @registration.email,
-     subject: "Anmeldung Erstiwochenende #{begin.year} - Daten vervollständigt",
+     subject: "Anmeldung Erstiwochenende #{@begin.year} - Daten vervollständigt",
+     content_type: "text/plain").deliver
+  end
+
+  def redeem_complete_mail(ticket)
+    @ticket = ticket
+    @begin = Time.new(2013,11,1,9,30,0)
+    mail(to: @ticket.email,
+     subject: "Anmeldung Erstiwochenende #{@begin.year}",
      content_type: "text/plain").deliver
   end
 
