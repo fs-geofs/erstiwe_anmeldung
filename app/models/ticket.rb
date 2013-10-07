9899d6deaf93e9d61c2f25133f6248ce82b10801
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-07 16:42:05 +0200

    slim tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,16 +1,5 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :user
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true, uniqueness: { case_sensitive: false }, on: 'update'
-
   before_create :generate_token
-  after_update :redeem_complete_message
-
-  def redeem_complete_message
-    RegistrationMailer.redeem_complete_mail(self)
-  end
 
   def generate_token
     chars = ('A'..'Z').to_a
