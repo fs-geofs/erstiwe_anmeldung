a0bc9db78b0715a46ecac57fa8fb2ccb77a1aac6
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-07 16:42:05 +0200

    slim tickets

--- db/migrate/20131004110955_create_tickets.rb
+++ db/migrate/20131004110955_create_tickets.rb
@@ -1,14 +1,11 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
-      t.belongs_to :user
       t.string :token
-      t.string :email
 
       t.timestamps
     end
 
     add_index :tickets, :token, unique: true
-    add_index :tickets, :email, unique: true
   end
 end
