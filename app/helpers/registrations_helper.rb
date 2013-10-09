1de15c01df942f7e84bb25f985aee18f757d6684
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-09 16:48:40 +0200

    pimped list

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,7 @@
 module RegistrationsHelper
+
+  def age_at(dob,at = Time.now)
+    now = at.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
