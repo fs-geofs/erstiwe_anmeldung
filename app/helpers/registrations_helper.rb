0302092a84e49af82df6f024a21e6e97caff6ecf
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-09 16:58:58 +0200

    dry in list

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -4,4 +4,13 @@ module RegistrationsHelper
     now = at.to_date
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
+
+  def gender_color user
+    case user.gender 
+      when 1 
+        safe_concat ' class="male"'
+      when 2
+        safe_concat ' class="female"'
+    end
+  end
 end
