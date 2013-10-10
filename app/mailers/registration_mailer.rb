82e9944f97a24344ce52217955e90ffc8ce7f81a
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-10 16:45:12 +0200

    registration mailer more dry

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,13 +5,7 @@ class RegistrationMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
     @token = token
     @config = AppConfig.first
-    opts = opts.merge({delivery_method: :smtp,
-     delivery_method_options: {
-       address: @config.mail_server,
-       port: @config.mail_port,
-       user_name: @config.mail_user,
-       password: @config.mail_password,
-       enable_starttls_auto: true}})
+    opts = mail_config opts
     devise_mail(record, :confirmation_instructions, opts)
   end
 
@@ -20,33 +14,31 @@ class RegistrationMailer < Devise::Mailer
     @config = AppConfig.first
     @begin = @config.beginning
     @end = @config.ending
-    mail(to: @registration.email,
-     from:  @config.mail_adress,
-     delivery_method: :smtp,
-     delivery_method_options: {
-       address: @config.mail_server,
-       port: @config.mail_port,
-       user_name: @config.mail_user,
-       password: @config.mail_password,
-       enable_starttls_auto: true},
+    mail(mail_config({to: @registration.email,
      subject: "Anmeldung Erstiwochenende #{@begin.year} - Daten vervollständigt",
-     content_type: "text/plain").deliver
+     content_type: "text/plain"})).deliver
   end
 
   def registration_destroyed_mail(user)
     @user = user
     @config = AppConfig.first
-    mail(to: @config.mail_adress,
-     from:  @config.mail_adress,
-     delivery_method: :smtp,
-     delivery_method_options: {
-       address: @config.mail_server,
-       port: @config.mail_port,
-       user_name: @config.mail_user,
-       password: @config.mail_password,
-       enable_starttls_auto: true},
+    mail(mail_config({to: @config.mail_adress,
      subject: "Anmeldung Erstiwochenende gelöscht - neues Ticket",
-     content_type: "text/plain").deliver
+     content_type: "text/plain"})).deliver
   end
+
+  private
+
+    def mail_config args
+      @config = AppConfig.first
+      {from:  @config.mail_adress,
+       delivery_method: :smtp,
+       delivery_method_options: {
+         address: @config.mail_server,
+         port: @config.mail_port,
+         user_name: @config.mail_user,
+         password: @config.mail_password,
+         enable_starttls_auto: true}}.merge(args)
+    end
 
 end
