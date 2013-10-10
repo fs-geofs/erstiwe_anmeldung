a1e2a9798333c4cb46ad0e856c100ca398991b1d
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-10 10:33:41 +0200

    create model for config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007100144) do
+ActiveRecord::Schema.define(version: 20131010082732) do
+
+  create_table "app_configs", force: true do |t|
+    t.datetime "beginning"
+    t.datetime "ending"
+    t.string   "mail_server"
+    t.string   "mail_user"
+    t.string   "mail_password"
+    t.integer  "mail_port"
+    t.boolean  "mail_auth"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tickets", force: true do |t|
     t.string   "token"
