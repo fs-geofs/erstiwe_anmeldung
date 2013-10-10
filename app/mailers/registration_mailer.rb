459755a8827c8d10aa28547b122b634c340c9f8d
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-10 14:43:52 +0200

    dynamic mailer

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,12 +1,36 @@
-class RegistrationMailer < ActionMailer::Base
-  default from: "fsgi@uni-muenster.de"
+class RegistrationMailer < Devise::Mailer
   helper RegistrationMailerHelper
+  helper :application
+
+  def confirmation_instructions(record, token, opts={})
+    @token = token
+    #binding.pry
+    @config = AppConfig.first
+    opts = opts.merge({delivery_method: :smtp,
+     delivery_method_options: {
+       address: @config.mail_server,
+       port: @config.mail_port,
+       user_name: @config.mail_user,
+       password: @config.mail_password,
+       enable_starttls_auto: true}})
+    binding.pry
+    devise_mail(record, :confirmation_instructions, opts)
+  end
 
   def registration_complete_mail(registration)
     @registration = registration
-    @begin = Time.new(2013,11,1,9,30,0)
-    @end = Time.new(2013,11,3,13,0,0)
+    @config = AppConfig.first
+    @begin = @config.beginning
+    @end = @config.ending
     mail(to: @registration.email,
+     from:  @config.mail_adress,
+     delivery_method: :smtp,
+     delivery_method_options: {
+       address: @config.mail_server,
+       port: @config.mail_port,
+       user_name: @config.mail_user,
+       password: @config.mail_password,
+       enable_starttls_auto: true},
      subject: "Anmeldung Erstiwochenende #{@begin.year} - Daten vervollständigt",
      content_type: "text/plain").deliver
   end
