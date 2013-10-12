363ec342fa4448962fd237a2f63dc2207cc2140d
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-12 22:35:02 +0200

    one too many 'def'. why isn't ruby telling me this?

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
-  prepend_before_filter :authenticate_scope!, :only => [:edit, :edit_email_password, :update, :destroy]
+  prepend_before_filter :authenticate_scope!, :only => [:edit, :edit_email_password, :update, :destroy, :list]
   before_filter :admin!, :only => :list
 
   def update
@@ -39,10 +39,9 @@ class RegistrationsController < Devise::RegistrationsController
     resource = current_user
   end
 
-  def
-
   def list
     @users = User.all
+    binding.pry
   end
 
   # DELETE /resource
