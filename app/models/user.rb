fs-geofs/erstiwe_anmeldung

commit e73584d84f6976379d7bb96cfdbd67ae34a3be12

fix for updating admin user

app/models/user.rb
@@ -13,7 +13,9 @@ class User < ActiveRecord::Base
 
 
   def map_ticket
-    self.ticket ||= Ticket.find_by(token: self.pre_token.upcase) 
+    if self.admin == false
+      self.ticket ||= ticket.find_by(token: self.pre_token.upcase)
+    end
   end
 
   def details_present?

commit 966f39e68396abd9aa65fda0f37c364daf4552c7

fix for bug introduced with e73584d84f

app/models/user.rb
@@ -13,8 +13,9 @@ class User < ActiveRecord::Base
 
 
   def map_ticket
+    binding.pry
     if self.admin == false
-      self.ticket ||= ticket.find_by(token: self.pre_token.upcase)
+      self.ticket ||= Ticket.find_by(token: self.pre_token.upcase)
     end
   end
 