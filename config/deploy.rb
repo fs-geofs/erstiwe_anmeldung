4476f750485d5438832d4191a8a5bf0f693852b1
ubergesundheit <ubergesundheit@googlemail.com>
2013-10-22 15:26:28 +0200

    testing cap linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :rbenv_custom_path, '/opt/rbenv'
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/home/deploy/.rbenv/shims:$PATH" }
