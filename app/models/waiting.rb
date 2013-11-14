8474b925c7145d2c01481ef4502608ec7ed82dc9
ubergesundheit <ubergesundheit@googlemail.com>
2013-11-14 15:54:09 +0100

    added validations

--- app/models/waiting.rb
+++ app/models/waiting.rb
@@ -3,4 +3,7 @@ class Waiting < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :registerable, :confirmable
 
+  email_regexp = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: email_regexp }
+
 end
