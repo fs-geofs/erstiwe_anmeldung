b2da9bd621ac642219e8ed0eb9e43fa6fdb4590f
ubergesundheit <ubergesundheit@googlemail.com>
2014-01-04 02:17:35 +0100

    mark new users from waiting list

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable, :confirmable
 
   before_validation :map_ticket
+  before_create :is_from_waiting_list?
 
   validates :fname, :lname, :street, :streetno, :zip, :place, :birthday, :ticket_id, :study, presence: true, if: :confirmed?, unless: :admin
   validates :ticket_id, presence: true, uniqueness: true, unless: :admin, unless: :withdrawn
@@ -22,6 +23,15 @@ class User < ActiveRecord::Base
 
   def details_present?
     self.fname.present? and self.lname.present? and self.streetno.present? and self.street.present? and self.zip.present? and self.place.present? and self.birthday.present? and self.ticket_id.present? and self.study.present?
+  end
+
+  def is_from_waiting_list?
+    if Waiting.count > 0
+      if Waiting.where(email: self.email)
+        self.from_waiting_list = true
+        Waiting.where(email: self.email).first.destroy
+      end
+    end
   end
 
   def active_for_authentication?
