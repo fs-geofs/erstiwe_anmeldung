5210786e25f8c5cb7207a1ecabbfdae67cf350af
ubergesundheit <ubergesundheit@googlemail.com>
2014-09-05 21:11:36 +0200

    set correct url for mails

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module ErstiweAnmeldung
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
 
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.action_mailer.default_url_options = { host: 'geofs.uni-muenster.de/erstiwochenende' }
   end
 end
